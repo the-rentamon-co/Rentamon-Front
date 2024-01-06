c7a09e08921fdb87b857b66da88d58e68069ae5b
ClassiCcOwl <khavari.7878@yahoo.com>
2024-01-06 18:38:18 +0330

    bug fixed

--- persian-datepicker.js
+++ persian-datepicker.js
@@ -3529,7 +3529,7 @@
                         results[1]["status"] === 200 &&
                         results[2]["status"] === 200 &&
                         results[3]["status"] === 200 &&
-                        results[4]["status"] === 200 &&
+                        results[4]["status"] === 200 
                         // results[5]["status"] === 200
                       ) {
                         for (let i = 0; i < availableDays.length; i++) {
