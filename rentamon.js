e179c01e218b73e5c31e61ba8f852ecb2ddb751f
ClassiCcOwl <khavari.7878@yahoo.com>
2024-01-17 09:30:48 +0330

    bug fixed

--- rentamon.js
+++ rentamon.js
@@ -1,7 +1,7 @@
 let apiHostMainUrl = "https://rentamon.chbk.run";
 
-let rentamon_user_id = document.querySelector("#rentamon_id").innerText.strip();
-let rentamon_room_id = document.querySelector("#rentamon_room_id").innerText.strip();
+let rentamon_user_id = document.querySelector("#rentamon_id").innerText.trim();
+let rentamon_room_id = document.querySelector("#rentamon_room_id").innerText.trim();
 
 let tehranTimeZone = "Asia/Tehran";
 let currentDate = new Date();
