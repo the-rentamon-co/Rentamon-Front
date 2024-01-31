d831219675e0964f274b9cb0f90fb9450a7c07fc
ClassiCcOwl <khavari.7878@yahoo.com>
2024-01-31 10:19:55 +0330

    babakhani select disabled

--- persian-datepicker.js
+++ persian-datepicker.js
@@ -2102,19 +2102,19 @@
                   /**
                    * @description time up btn click event
                    */
-                  // $(document).on('click', '#' + that.model.view.id + ' .up-btn', function () {
-                  //     var timekey = $(this).data('time-key');
-                  //     that.timeUp(timekey);
-                  //     that.model.options.onSelect(that.model.state.selected.unixDate);
-                  // });
-                  // /**
-                  //  * @description time down btn click event
-                  //  */
-                  // $(document).on('click', '#' + that.model.view.id + ' .down-btn', function () {
-                  //     var timekey = $(this).data('time-key');
-                  //     that.timeDown(timekey);
-                  //     that.model.options.onSelect(that.model.state.selected.unixDate);
-                  // });
+                  $(document).on('click', '#' + that.model.view.id + ' .up-btn', function () {
+                      var timekey = $(this).data('time-key');
+                      that.timeUp(timekey);
+                      that.model.options.onSelect(that.model.state.selected.unixDate);
+                  });
+                  /**
+                   * @description time down btn click event
+                   */
+                  $(document).on('click', '#' + that.model.view.id + ' .down-btn', function () {
+                      var timekey = $(this).data('time-key');
+                      that.timeDown(timekey);
+                      that.model.options.onSelect(that.model.state.selected.unixDate);
+                  });
                 }
 
                 /**
@@ -2124,40 +2124,40 @@
                   /**
                    * @description days click event
                    */
-                  $(document).on(
-                    "click",
-                    "#" +
-                      that.model.view.id +
-                      " .datepicker-day-view td:not(.disabled)",
-                    function () {
-                      var thisUnix = $(this).data("unix"),
-                        mustRender = void 0;
-                      that.model.state.setSelectedDateTime("unix", thisUnix);
-                      if (
-                        that.model.state.selected.month !==
-                        that.model.state.view.month
-                      ) {
-                        mustRender = true;
-                      } else {
-                        mustRender = false;
-                      }
-                      that.model.state.setViewDateTime(
-                        "unix",
-                        that.model.state.selected.unixDate
-                      );
-                      if (that.model.options.autoClose) {
-                        that.model.view.hide();
-                        that.model.options.onHide(that);
-                      }
-                      if (mustRender) {
-                        that.model.view.render();
-                      } else {
-                        that.model.view.markSelectedDay();
-                      }
-                      that.model.options.dayPicker.onSelect(thisUnix);
-                      that.model.options.onSelect(thisUnix);
-                    }
-                  );
+                  // $(document).on(
+                  //   "click",
+                  //   "#" +
+                  //     that.model.view.id +
+                  //     " .datepicker-day-view td:not(.disabled)",
+                  //   function () {
+                  //     var thisUnix = $(this).data("unix"),
+                  //       mustRender = void 0;
+                  //     that.model.state.setSelectedDateTime("unix", thisUnix);
+                  //     if (
+                  //       that.model.state.selected.month !==
+                  //       that.model.state.view.month
+                  //     ) {
+                  //       mustRender = true;
+                  //     } else {
+                  //       mustRender = false;
+                  //     }
+                  //     that.model.state.setViewDateTime(
+                  //       "unix",
+                  //       that.model.state.selected.unixDate
+                  //     );
+                  //     if (that.model.options.autoClose) {
+                  //       that.model.view.hide();
+                  //       that.model.options.onHide(that);
+                  //     }
+                  //     if (mustRender) {
+                  //       that.model.view.render();
+                  //     } else {
+                  //       that.model.view.markSelectedDay();
+                  //     }
+                  //     that.model.options.dayPicker.onSelect(thisUnix);
+                  //     that.model.options.onSelect(thisUnix);
+                  //   }
+                  // );
                 }
 
                 /**
