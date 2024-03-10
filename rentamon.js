2fb2d24d06116e0323ad771c3abe5e404d65080c
ownen <mr.joker.mr.joke@gmail.com>
2024-03-10 18:17:00 +0330

    change red background-color to red border

--- rentamon.js
+++ rentamon.js
@@ -1421,12 +1421,12 @@ function check_is_valid(id, pop_up_id) {
 function isActiveHandler(id, isRed) {
   if (isRed) {
     document.querySelector(`${id}`).style.display = "block";
-    document.querySelector(`${id} div div div`).style.backgroundColor = "#d71d1d";
+    document.querySelector(`${id} div div div`).style.border = "4px #d71d1d solid";
     document.querySelector(`${id} div div div`).style.borderRadius = "5px";
     document.querySelector(`${id} div div div img`).style.filter = "grayscale(1)";
   } else if (isRed === false) {
     document.querySelector(`${id}`).style.display = "block";
-    document.querySelector(`${id} div div div`).style.border = "3px #0c9d61 solid";
+    document.querySelector(`${id} div div div`).style.border = "4px #0c9d61 solid";
     document.querySelector(`${id} div div div`).style.borderRadius = "5px";
   }
 }
