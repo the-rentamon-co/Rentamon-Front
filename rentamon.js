b585778a6908a33a5576e3baff9046d4f29e4eb0
ClassiCcOwl <khavari.7878@yahoo.com>
2024-03-18 09:58:18 +0330

    until ordibehest

--- rentamon.js
+++ rentamon.js
@@ -1323,7 +1323,7 @@ $(window).on("load", function () {
     },
     inline: true,
     minDate: new persianDate().startOf("day"),
-    maxDate: new persianDate([1403, 1, 31]),
+    maxDate: new persianDate([1403, 2, 31]),
     // maxDate: new persianDate().add("years", 1).month(2).endOf("month"),
     // maxDate: new persianDate().
     // maxDate: new persianDate()
