bfcac6dfc871bd7ad7d045c0ccb6eb3b0f08eb87
novartis-nk <anikoo12@yahoo.com>
2024-04-14 17:40:53 +0330

    bug fix

--- rentamon.js
+++ rentamon.js
@@ -318,38 +318,12 @@ function rentamonApiCaller(
         },
       },
       beforeSend: function () {
-        if (action == "discount"){  
-          if(website == 'homsa' || website == 'jabama' || website || 'otaghak'){
-            console.log("came here in conditions!")
+        if (action == "discount"){
+          const non_discount = ['otaghak', 'mizboon', 'jajiga', 'shab', 'mihmansho'];
+          non_discount.forEach(className => {
           document
-            .querySelectorAll(`.website_row.${website}`)
-            .forEach((a) => (a.style.display = "block"));
-          document
-            .querySelectorAll(`.elementor-section.${website} .status_true`)
-            .forEach((b) => (b.style.display = "none"));
-          document
-            .querySelectorAll(`.elementor-section.${website} .status_false`)
-            .forEach((c) => (c.style.display = "none"));
-          document
-            .querySelectorAll(`.elementor-section.${website} .status_pending`)
-            .forEach((c) => (c.style.display = "block"));
-          console.log("Anything happend!? in discount condition");
-          }
-          else{
-          document
-            .querySelectorAll(`.website_row.${website}`)
-            .forEach((a) => (a.style.display = "none"));
-          document
-            .querySelectorAll(`.elementor-section.${website} .status_true`)
-            .forEach((b) => (b.style.display = "none"));
-          document
-            .querySelectorAll(`.elementor-section.${website} .status_false`)
-            .forEach((c) => (c.style.display = "none"));
-          document
-            .querySelectorAll(`.elementor-section.${website} .status_pending`)
-            .forEach((c) => (c.style.display = "none"));
-            console.log("Anything happend in Else!?");
-          }
+            .querySelectorAll(`.website_row.${className}`)
+            .forEach(element => {element.style.display = "none";});});
         }
         else{
         document
