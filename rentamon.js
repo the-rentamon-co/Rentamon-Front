ab02f9ae0d4cdb3e5836aaf8b000109664081e2e
novartis-nk <anikoo12@yahoo.com>
2024-04-16 14:46:56 +0330

    front pop up discount change

--- rentamon.js
+++ rentamon.js
@@ -318,9 +318,7 @@ function rentamonApiCaller(
         },
       },
       beforeSend: function () {
-
-        if (action !== "discount"){
-          document
+        document
             .querySelectorAll(`.website_row.${website}`)
             .forEach((a) => (a.style.display = "block"));
           document
@@ -333,31 +331,46 @@ function rentamonApiCaller(
             .querySelectorAll(`.elementor-section.${website} .status_pending`)
             .forEach((c) => (c.style.display = "block"));
           console.log("Anything happend after condition!?");
+
+        // if (action !== "discount"){
+        //   document
+        //     .querySelectorAll(`.website_row.${website}`)
+        //     .forEach((a) => (a.style.display = "block"));
+        //   document
+        //     .querySelectorAll(`.elementor-section.${website} .status_true`)
+        //     .forEach((b) => (b.style.display = "none"));
+        //   document
+        //     .querySelectorAll(`.elementor-section.${website} .status_false`)
+        //     .forEach((c) => (c.style.display = "none"));
+        //   document
+        //     .querySelectorAll(`.elementor-section.${website} .status_pending`)
+        //     .forEach((c) => (c.style.display = "block"));
+        //   console.log("Anything happend after condition!?");
           
-        }
-        else{
-          const non_discount = ['mizboon', 'jajiga', 'shab', 'mihmansho'];
-          const with_discount = ['jabama', 'homsa', 'otaghak'];
-          with_discount.forEach(className => {
-            document
-            .querySelectorAll(`.website_row.${className}`)
-            .forEach(element => {element.style.display = "block";});
-            document
-            .querySelectorAll(`.elementor-section.${className} .status_pending`)
-            .forEach((c) => (c.style.display = "block"));
-            document
-            .querySelectorAll(`.elementor-section.${className} .status_false`)
-            .forEach((c) => (c.style.display = "none"));
-            document
-            .querySelectorAll(`.elementor-section.${className} .status_true`)
-            .forEach((c) => (c.style.display = "none"));
-          });
-          non_discount.forEach(className => {
-          document
-            .querySelectorAll(`.website_row.${className}`)
-            .forEach(element => {element.style.display = "none";});
-            });
-        }
+        // }
+        // else{
+        //   const non_discount = ['mizboon', 'jajiga', 'shab', 'mihmansho'];
+        //   const with_discount = ['jabama', 'homsa', 'otaghak'];
+        //   with_discount.forEach(className => {
+        //     document
+        //     .querySelectorAll(`.website_row.${className}`)
+        //     .forEach(element => {element.style.display = "block";});
+        //     document
+        //     .querySelectorAll(`.elementor-section.${className} .status_pending`)
+        //     .forEach((c) => (c.style.display = "block"));
+        //     document
+        //     .querySelectorAll(`.elementor-section.${className} .status_false`)
+        //     .forEach((c) => (c.style.display = "none"));
+        //     document
+        //     .querySelectorAll(`.elementor-section.${className} .status_true`)
+        //     .forEach((c) => (c.style.display = "none"));
+        //   });
+        //   non_discount.forEach(className => {
+        //   document
+        //     .querySelectorAll(`.website_row.${className}`)
+        //     .forEach(element => {element.style.display = "none";});
+        //     });
+        // }
       },
       success: function (response) {
         console.log(website, response, status);
