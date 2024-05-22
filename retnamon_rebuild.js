cbf20f29e57e38d7e848e759ab75d7864f55ad85
Alireza <anikoo12@yahoo.com>
2024-05-22 13:59:31 +0330

    putting the new changes for the fron up in the air

--- retnamon_rebuild.js
+++ retnamon_rebuild.js
@@ -1,5 +1,12 @@
 let apiHostMainUrl = "https://api-rentamon.liara.run";
 
+url = {
+  block: apiHostMainUrl + "/setblock/",
+  unblock: apiHostMainUrl + "/setunblock/",
+  calendar: apiHostMainUrl + "/getcalendar",
+  price: apiHostMainUrl + "/setprice/",
+  discount: apiHostMainUrl + "/setdiscount/",
+}
 // this is calendar config
 
 // for changing max date change value in maxDate: new persianDate
@@ -114,53 +121,194 @@ $(window).on("load", function () {
   }
 });
 
-
-// this is the main function that fetches data from websites based on calendar
 async function rentamoning() {
-  document
-    .querySelectorAll("form")
-    .forEach((form) => form.removeEventListener("submit", rentamoning));
+  // Remove the event listener from all forms to prevent multiple submissions
+  document.addEventListener('DOMContentLoaded', checkAuthOnLoad)
+  document.querySelectorAll("form").forEach(form => {
+    form.removeEventListener("submit", rentamoning);
+  });
 
-  // starts loading when this function is called
+  // Display the loading overlay
   document.querySelector(".loading-overlay-calendar").style.display = "flex";
 
-  // resets every action input
-  document
-    .querySelectorAll('input[name="block"]')
-    .forEach((i) => (i.checked = false));
-  var availableDays = [];
-
-  // selecting start and end of each days range
-  var allTds = document.querySelectorAll(
-    ".datepicker-day-view td:not(.disabled)"
-  );
-  allTds.forEach((td) => {
-    if (!td.firstElementChild.classList.contains("other-month")) {
-      availableDays.push(td);
-    } else {
-      td.classList.add("other-month");
-    }
+  // Reset all inputs with the name "block"
+  document.querySelectorAll('input[name="block"]').forEach(input => {
+    input.checked = false;
   });
+
+  // Get all available days in the datepicker
+  const availableDays = Array.from(
+    document.querySelectorAll(".datepicker-day-view td:not(.disabled)")
+  ).filter(td => {
+    const isOtherMonth = td.firstElementChild.classList.contains("other-month");
+    if (isOtherMonth) {
+      td.classList.add("other-month");
+      return false;
+    }
+    return true;
+  });
+
+  // If there are available days, generate the date range
   if (availableDays.length > 0) {
-    var days = document.querySelectorAll(
-      ".datepicker-plot-area-inline-view .table-days td:not(.disabled) span:not(.other-month):not(.reserved):not(.price)"
-    );
-    var range = [
-      new persianDate(
-        parseInt(availableDays[0].getAttribute("data-unix"))
-      ).format("YYYY-MM-DD"),
-      new persianDate(
-        parseInt(
-          availableDays[availableDays.length - 1].getAttribute("data-unix")
-        )
-      ).format("YYYY-MM-DD"),
-      new persianDate(
-        parseInt(
-          availableDays[availableDays.length - 1].getAttribute("data-unix")
-        )
-      )
-        .add("day", 1)
-        .format("YYYY-MM-DD"),
+    const firstDayUnix = parseInt(availableDays[0].getAttribute("data-unix"));
+    const lastDayUnix = parseInt(availableDays[availableDays.length - 1].getAttribute("data-unix"));
+
+    const range = [
+      new persianDate(firstDayUnix).format("YYYY-MM-DD"),
+      new persianDate(lastDayUnix).format("YYYY-MM-DD"),
+      new persianDate(lastDayUnix).add("day", 1).format("YYYY-MM-DD")
     ];
+  }
+}
+
+
+
+// this function ckecks witch action btn is ckecked
+function checkAction() {
+  let action = document.querySelector('input[name="block"]:checked');
+  if (action) {
+    if (action.value === "block") {
+      blockBtnClicked();
+    } else if (action.value === "reserve") {
+      reserveOther();
+    } else if (action.value === "unblock") {
+      unblockBtnClicked();
+    }
+  } else {
+    // alert(messages.notSelectedDay);
+  }
+}
+
+function priceBtnClicked() {
+  let selected = document.querySelectorAll(".selected");
+  if (selected.length > 0) {
+    var price_div = document.createElement("div");
+    price_div.style.display = "none";
+    price_div.className = "price-submit";
+    document.body.appendChild(price_div);
+    price_div.click();
+  } else {
+    alert(messages.notSelectedDay);
+  }
+}
+
+
+// this function is called when block option is selected
+// if there are selected days, it starts requesting for block to each website
+async function blockBtnClicked() {
+  document.querySelector(".loading-overlay-calendar").style.display = "flex";
+
+  console.log(JSON.stringify(regweb));
+  let selected = document.querySelectorAll(".selected");
+  let selectedDate = [];
+  if (selected.length > 0) {
+    selected.forEach((z) => {
+      z.classList.remove("selected");
+      selectedDate.push(
+        new persianDate(parseInt(z.getAttribute("data-unix"))).format(
+          "YYYY-MM-DD"
+        )
+      );
+    });
+    var response_status = document.querySelector(".response_status");
+    if (response_status) {
+      document.querySelector(".response_status_pop a").click();
+    }
+    const apicalls = [];
+
+    
+    if(regWebsites.includes('otaghak')){
+      apicalls.push(rentamonApiCaller(
+        (website = "otaghak"),
+        (data = {
+          rentamon_room_id: routes["otaghak"]["room"],
+          unblockDays: null,
+          blockDays: selectedDate.join(","),
+        }),
+        (action = "block")
+      ))
+    }
+    
+    const resps = await Promise.all(apicalls);
+    await rentamoning();
+  } else {
+    alert(messages.notSelectedDay);
+    document.querySelector(".loading-overlay-calendar").style.display = "none";
+  }
+}
+
+// this function is called when unblock option is selected
+// if there are selected days, it starts requesting for unblock to each website
+async function unblockBtnClicked() {
+
+  document.querySelector(".loading-overlay-calendar").style.display = "flex";
+  let selected = document.querySelectorAll(".selected");
+  let selectedDate = [];
+  if (selected.length > 0) {
+    selected.forEach((z) => {
+      z.classList.remove("selected");
+      selectedDate.push(
+        new persianDate(parseInt(z.getAttribute("data-unix"))).format(
+          "YYYY-MM-DD"
+        )
+      );
+    });
+    var response_status = document.querySelector(".response_status");
+
+    if (response_status) {
+      document.querySelector(".response_status_pop a").click();
+    }
+    // adding api calls if user has registered in the website
+    const apicalls = [
+      rentamonApiCaller(
+        (website = "otherv2"),
+        (data = {
+          action: "available",
+          rentamon_room_id: routes["otherv2"]["room"],
+          days: selectedDate.join(","),
+        }),
+        (action = "blockUnblock")
+      ),
+    ]
+
+    if(regWebsites.includes('otaghak')){
+      apicalls.push(rentamonApiCaller(
+        (website = "otaghak"),
+        (data = {
+          rentamon_room_id: routes["otaghak"]["room"],
+          unblockDays: selectedDate.join(","),
+          blockDays: null,
+        }),
+        (action = "unblock")
+      ))
+    }
+
+    console.log(regweb);
+    const resps = await Promise.all(apicalls);
+    await rentamoning();
+  } else {
+    alert(messages.notSelectedDay);
+    document.querySelector(".loading-overlay-calendar").style.display = "none";
+  }
+}
+
+async function checkAuthOnLoad() {
+  const accessToken = getCookie('auth_token');
+  const refreshToken = getCookie('refresh_token');
+
+  if (!accessToken || !refreshToken) {
+    return false;
+  }
+
+  const isAccessTokenValid = await verifyToken(accessToken, 'access');
+  if (!isAccessTokenValid) {
+    const isRefreshTokenValid = await verifyToken(refreshToken, 'refresh');
+    if (!isRefreshTokenValid) {
+      return false;
+    } else {
+      return false // TODO : adding the refresh token handler 
+    }
+  }else{
+    return true
   }
 }
