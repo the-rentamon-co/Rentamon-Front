422ea822d854bca8a4b05704f55558fc56a892aa
ownen <mr.joker.mr.joke@gmail.com>
2024-05-29 20:31:54 +0330

    make condition for all forEach

--- rentamon_rebuild.js
+++ rentamon_rebuild.js
@@ -195,13 +195,14 @@ function setAvailableHelper(elements, selectedDate = "") {
 }
 function setBookedkHelper(elements) {
   elements.forEach((elm) => {
-    if (elm.parentElement)
+    if (elm.parentElement) {
       if (elm.parentElement.classList)
         elm.parentElement.classList.remove("discounted-days");
-    elm.parentElement.classList.add("booked-days");
-    elm.parentElement.querySelector(".reserved").innerHTML = reservedViewer(
-      elm.website
-    );
+      elm.parentElement.classList.add("booked-days");
+      elm.parentElement.querySelector(".reserved").innerHTML = reservedViewer(
+        elm.website
+      );
+    }
   });
 }
 
