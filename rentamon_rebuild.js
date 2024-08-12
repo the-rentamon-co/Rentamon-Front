e6206d38ea0c4e57080ea64b6d1fc10b54fabe0c
Alireza <anikoo12@yahoo.com>
2024-08-12 14:11:19 +0330

    bug fix for block

--- rentamon_rebuild.js
+++ rentamon_rebuild.js
@@ -742,10 +742,10 @@ async function blockBtnClicked() {
       (property_id = propertyIdFromQueryParams)
     );
     console.log("Response Data: ", final_response);
-    status_responses = Object.values(final_response.data.final_status);
+    status_responses = Object.values(final_response.data);
     console.log("status Response Data: ", status_responses);
     // console.log(spans);
-    if (status_responses.every((rep) => rep === true)) {
+    if (status_responses.every((rep) => rep.final_status === true)) {
       setBlockHelper(spans);
     }
     setStatusStyleV2(final_response.data);
