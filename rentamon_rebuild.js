2e2fd151401317d09c7ac9ec3f00a229afaeabfc
Alireza <anikoo12@yahoo.com>
2024-08-15 12:39:25 +0330

    bug fix

--- rentamon_rebuild.js
+++ rentamon_rebuild.js
@@ -291,14 +291,14 @@ async function get_user_info() {
       },
     }
   );
-  // try {
-  //   const res = await response.json()
-  //   console.log(res.user_info.phone_number)
-  //   clarity('set', 'phone_number', response.user_info.phone_number);
-  // } catch (error) {
-  //   console.log("got here in error")
+  try {
+    const res = await response.json()
+    console.log(res.user_info.phone_number)
+    clarity('set', 'phone_number', res.user_info.phone_number);
+  } catch (error) {
+    console.log("got here in error")
 
-  // }
+  }
   
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
