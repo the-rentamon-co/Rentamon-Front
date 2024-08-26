a8e8ecb5cf4600cb24160802794869a577d60997
Novartis <anikoo12@yahoo.com>
2024-08-26 17:59:00 +0330

    bug fix

--- rentamon_rebuild.js
+++ rentamon_rebuild.js
@@ -451,14 +451,18 @@ async function rentamoning() {
           websites.forEach((website)=> {
             const widget = websiteWidgets[website];
             if(website == 'jabama'){
-              if(jabama_status.jabama_response){
+              if(jabama_status.jabama_response == true){
                 isActiveHandler(widget.icon_selector, false);
+                console.log("got here in jabama true : ", jabama_status.jabama_response)
               }else{
                 isActiveHandler(widget.icon_selector, true);
                 check_is_valid(widget.icon_selector, widget.popup_id_selector);
+                console.log("got here in jabama false : ", jabama_status.jabama_response)
+
               }
-            }
+            }else{
             isActiveHandler(widget.icon_selector, false);
+            }
           })
         }catch{
           console.log("Error in handling website statuses")
