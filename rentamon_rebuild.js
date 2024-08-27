8263508abfed53be33f945863594b3ffbba64f4a
Novartis <anikoo12@yahoo.com>
2024-08-27 09:46:26 +0330

    adding another month (Aban)

--- rentamon_rebuild.js
+++ rentamon_rebuild.js
@@ -1170,7 +1170,7 @@ $(".inline").pDatepicker({
   },
   inline: true,
   minDate: new persianDate().startOf("day"),
-  maxDate: new persianDate([1403, 7, 30]),
+  maxDate: new persianDate([1403, 8, 30]),
   // maxDate: new persianDate().add("years", 1).month(2).endOf("month"),
   // maxDate: new persianDate().
   // maxDate: new persianDate()
