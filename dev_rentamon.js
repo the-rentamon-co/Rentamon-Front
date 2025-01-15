96718bd539b4b5d10fec8a016c6110ebcf979add
Novartis <anikoo12@yahoo.com>
2025-01-15 15:45:36 +0330

    adding Farvardin to calendar

--- dev_rentamon.js
+++ dev_rentamon.js
@@ -1243,7 +1243,7 @@ $(".inline").pDatepicker({
   },
   inline: true,
   minDate: new persianDate().startOf("day"),
-  maxDate: new persianDate([1403, 11, 30]),
+  maxDate: new persianDate([1404, 1, 31]),
   // maxDate: new persianDate().add("years", 1).month(2).endOf("month"),
   // maxDate: new persianDate().
   // maxDate: new persianDate()
