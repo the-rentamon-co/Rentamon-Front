93245604b0c77ea8ecec6d65acbb90170851e823
parsa khosravi <118853774+parsahvi@users.noreply.github.com>
2025-06-12 09:22:39 +0330

    Update dev_rentamon.js

--- dev_rentamon.js
+++ dev_rentamon.js
@@ -1300,7 +1300,7 @@ $(".inline").pDatepicker({
   },
   inline: true,
   minDate: new persianDate().startOf("day"),
-  maxDate: new persianDate([1404, 5, 31]),
+  maxDate: new persianDate([1404, 6, 31]),
   // maxDate: new persianDate().add("years", 1).month(2).endOf("month"),
   // maxDate: new persianDate().
   // maxDate: new persianDate()
